3ff9d2af511ec89d2358279c69fe4e259122b01c
IsaAfandiyev <isa.efendiyev@gmail.com>
2022-06-06 19:20:58 +0400

    added easter egg for bulka

---
+++ scripts/bulka.js
@@ -0,0 +1,28 @@
+const bulka = document.getElementById('bulka');
+const cucumber = document.getElementById('cucumber');
+const gameScreen = document.getElementById('game');
+const endGameScreen = document.getElementById('endGameScreen');
+const jumpKeyName = 'Space';
+const jumpClassName = 'jump';
+
+const jump = () => {
+    if (bulka.classList !== jumpClassName) {
+        bulka.classList.add(jumpClassName);
+
+        setTimeout(() => bulka.classList.remove(jumpClassName), 300);
+    }
+};
+
+const isAlive = setInterval(() => {
+    const bulkaTop = parseInt(window.getComputedStyle(bulka).getPropertyValue('top'));
+    const cucumberLeft = parseInt(window.getComputedStyle(cucumber).getPropertyValue('left'));
+
+    if (cucumberLeft < 50 && cucumberLeft > 0 && bulkaTop >= 140) {
+        gameScreen.classList.add('gameOver');
+        endGameScreen.classList.add('active');
+    };
+}, 10);
+
+document.addEventListener('keydown', ({ code }) => {
+    if (code === jumpKeyName) jump();
+});
